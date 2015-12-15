c2ebaa2562fa69fb53b435c982c4ce747d377517
me <me@swdevelop.net>
2015-12-15 12:38:34 +0200

    Fix str method.

--- example/persons/models.py
+++ example/persons/models.py
@@ -6,5 +6,5 @@ class Person(models.Model):
     email = models.EmailField(max_length=128, null=True, blank=True)
 
     def __str__(self):
-        return '%s %s' % (self.short_name, 
-                          '(%s)' % self.full_name if self.full_name else '')
+        return '%s%s' % (self.short_name, 
+                          ' (%s)' % self.full_name if self.full_name else '')
