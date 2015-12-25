9eed07aaad95eecc72f8729d21b3b31b449e5a90
me <me@swdevelop.net>
2015-12-25 15:39:20 +0200

    Remove example_mongodb.

--- example_mongodb/persons/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class AddPersonForm(forms.Form):
-    short_name = forms.CharField(max_length=32, required=True)
-    full_name = forms.CharField(max_length=128, required=False)
-    email = forms.EmailField(max_length=128, required=False)
-
-class RemovePersonForm(forms.Form):
-    short_name_substr = forms.CharField(max_length=32, required=True)
